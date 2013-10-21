dadc3b4e10841a7bc0d77e7cff9875391f6bbdcc
dmf21 <dmfkun@gmail.com>
2013-10-22 01:51:24 +0300

    Added Fibonacci Sequence

---
+++ Numbers/fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+        
+number = int(raw_input("Enter number: "))
+
+for i in range(0, number):
+    print fibonacci(i)
