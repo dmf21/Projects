9b55fe68bb502c2452ffc84492050e40cacf83b1
dmf21 <dmfkun@gmail.com>
2013-10-23 01:03:25 +0300

    prime factorization

--- Numbers/primefactorization.py
+++ Numbers/primefactorization.py
@@ -1,14 +1,17 @@
 number = int(raw_input("Enter a number: "))
 
-#check if a number is prime (naive algorithm)
 def is_prime(n):
-    sum = 0
-    for x in range(1, n):
-        if n%x == 0:
-            sum = sum + 1
-    if sum < 2:
-        return True
-    else:
-        return False
-
+    for i in range(2, n):
+        if i % n == 0:
+            return False
+    return True
+    
 factors = []
+
+for i in range(2, number +1):
+    while number % i == 0:
+        if is_prime(i):
+            factors.append(i)
+            number /= i
+
+print factors
